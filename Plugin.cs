5fcaf1fd22a60fa5b11951eede607f4423b528a9
Fuann-Kinoko <838832306@qq.com>
2026-01-18 00:01:56 +0800

    add gitignore

--- Plugin.cs
+++ Plugin.cs
@@ -3,14 +3,13 @@ using BepInEx.Logging;
 
 namespace TranslationPlugin;
 
-[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[BepInPlugin("org.bepinex.plugins.EHTranslationPlugin", "Translation Plugin", "1.0.0.0")]
 public class Plugin : BaseUnityPlugin
 {
     internal static new ManualLogSource Logger;
-        
+
     private void Awake()
     {
-        // Plugin startup logic
         Logger = base.Logger;
         Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
     }
